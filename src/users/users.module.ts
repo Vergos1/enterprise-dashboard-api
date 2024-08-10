68fa4421657e95d4d5796a4c605b54c322cde160
vhuser <vh@controlledexplosion.net>
2024-08-11 01:43:52 +0300

    chore: Add TypeOrmModule and AppLoggerModule to UsersModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
+import { AppLoggerModule } from 'src/app-logger/app-logger.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity]), AppLoggerModule],
   providers: [UsersService],
   controllers: [UsersController],
+  exports: [UsersService],
 })
 export class UsersModule {}
