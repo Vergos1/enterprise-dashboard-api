a32a689f502f1d54a5ac4e24ab12d8c236547541
vhuser <vh@controlledexplosion.net>
2024-08-11 01:42:44 +0300

    chore: Add auth and database config types

--- src/config/config.types.ts
+++ src/config/config.types.ts
@@ -1,5 +1,7 @@
 import { DatabaseConfig } from '../database/config/database.config.types';
+import { AuthConfig } from '../auth/config/auth-config.type';
 
 export type AllConfigType = {
   database: DatabaseConfig;
+  auth: AuthConfig;
 };
