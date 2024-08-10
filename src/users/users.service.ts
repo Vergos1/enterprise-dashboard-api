36a23276e0e2fb2188dba5a3076c23b316e2f85d
vhuser <vh@controlledexplosion.net>
2024-08-11 01:44:30 +0300

    Update UsersService to include CRUD

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  async findAll(): Promise<UserEntity[]> {
+    return this.userRepository.find();
+  }
+
+  async findOne(id: string): Promise<UserEntity> {
+    return this.userRepository.findOne({ where: { id } });
+  }
+
+  async findOneByEmail(email: string): Promise<UserEntity | undefined> {
+    return this.userRepository.findOne({ where: { email } });
+  }
+
+  async create(user: Partial<UserEntity>): Promise<UserEntity> {
+    return this.userRepository.save(user);
+  }
+
+  async update(
+    id: string,
+    updateUserDto: Partial<UserEntity>,
+  ): Promise<UserEntity> {
+    await this.userRepository.update(id, updateUserDto);
+    return this.findOne(id);
+  }
+
+  async remove(id: string): Promise<void> {
+    await this.userRepository.delete(id);
+  }
+}
