0fb3cfac5c234840574a972c1bf5c43bdb11164a
vhuser <vh@controlledexplosion.net>
2024-08-10 19:22:46 +0300

    chore: Add roles entity, enum, module, and service

---
+++ src/roles/entities/roles.entity.ts
@@ -0,0 +1,26 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToMany,
+  JoinTable,
+} from 'typeorm';
+import { UserEntity } from '../../users/entities/user.entity';
+import { Role as RoleEnum } from '../roles.enum';
+
+@Entity()
+export class Role {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'enum',
+    enum: RoleEnum,
+    default: RoleEnum.Guest,
+  })
+  name: RoleEnum;
+
+  @ManyToMany(() => UserEntity)
+  @JoinTable()
+  users: UserEntity[];
+}
