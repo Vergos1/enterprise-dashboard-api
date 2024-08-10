94adc249e3d601f4940dc80f497f1c32cd54111e
vhuser <vh@controlledexplosion.net>
2024-08-11 01:42:31 +0300

    chore: Add JWTGuard and LocalGuard for authentication

---
+++ src/auth/guards/local.guard.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ExecutionContext } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class LocalGuard extends AuthGuard('local') {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    return super.canActivate(context);
+  }
+}
---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,35 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { UsersService } from '../../users/users.service';
+import { AllConfigType } from '../../config/config.types';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+type JwtPayload = {
+  id: string;
+};
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly usersService: UsersService,
+    configService: ConfigService<AllConfigType>,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get('auth', { infer: true }).secret,
+      ignoreExpiration: false,
+    });
+  }
+  async validate(payload: JwtPayload) {
+    const user = await this.usersService.findOne(payload.id);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    delete user.password;
+    return user;
+  }
+}
