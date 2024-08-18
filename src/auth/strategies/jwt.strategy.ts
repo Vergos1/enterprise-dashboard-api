db999579e3629101ec194a0d71355b467f844986
vhuser <vh@controlledexplosion.net>
2024-08-18 16:10:18 +0300

    feat: Add check for user status before authentication

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,10 +1,16 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
 import { UsersService } from '../../users/users.service';
 import { AllConfigType } from '../../config/config.types';
 import * as dotenv from 'dotenv';
+import { ERROR_MESSAGES } from '../../utils/constants/all-constants';
+import { UserStatus } from '../../users/constants/user-status.enum';
 dotenv.config();
 
 type JwtPayload = {
@@ -25,6 +31,10 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
   async validate(payload: JwtPayload) {
     const user = await this.usersService.findOne({ id: payload.id });
+
+    if (user.status !== UserStatus.Active) {
+      throw new ForbiddenException(ERROR_MESSAGES.DONT_HAVE_PERMISSION);
+    }
 
     if (!user) {
       throw new UnauthorizedException();
