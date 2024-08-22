a4a44c5f8c59db617a3b6e16b4143c534cbf030c
vhuser <vh@controlledexplosion.net>
2024-08-22 22:44:47 +0300

    refactor: Add bearer authentication and authorization to QuestionsController

--- src/questions/questions.controller.ts
+++ src/questions/questions.controller.ts
@@ -13,15 +13,22 @@ import {
   ApiOperation,
   ApiTags,
   ApiNotFoundResponse,
+  ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
 import { UuidParamDto } from '../categories/dto/uuid-param.dto';
 import { QuestionsService } from './questions.service';
 import { QuestionDto } from './dto/question.dto';
 import { CreateQuestionDto } from './dto/create-question.dto';
 import { UpdateQuestionDto } from './dto/update-question.dto';
+import { UseGuards } from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { JwtGuard } from 'src/auth/guards/jwt.quard';
 
 @ApiTags('Questions')
 @Controller('questions')
+@UseGuards(JwtGuard)
+@ApiBearerAuth()
+@ApiUnauthorizedResponse({ description: 'Unauthorized' })
 export class QuestionsController {
   constructor(private readonly questionsService: QuestionsService) {}
 
