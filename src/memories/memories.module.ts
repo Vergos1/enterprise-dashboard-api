572cfcf8d882d85125cbcb0a03084542f0bb8bb5
vhuser <vh@controlledexplosion.net>
2024-08-29 01:14:02 +0300

    refactor: Update MemoriesModule to include MemoriesController and MemoriesRepositoryModule

--- src/memories/memories.module.ts
+++ src/memories/memories.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MemoriesService } from './memories.service';
+import { MemoriesController } from './memories.controller';
+import { MemoriesRepositoryModule } from './repositories/memories.repository.module';
 
 @Module({
+  imports: [MemoriesRepositoryModule],
   providers: [MemoriesService],
+  controllers: [MemoriesController],
 })
 export class MemoriesModule {}
